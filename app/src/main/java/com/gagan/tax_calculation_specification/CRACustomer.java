76958041f195d3917d743a74a0d9dd9dbb690283
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-08 15:40:11 -0400

    [gk]implementing parceable

--- app/src/main/java/com/gagan/tax_calculation_specification/CRACustomer.java
+++ app/src/main/java/com/gagan/tax_calculation_specification/CRACustomer.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class CRACustomer  implements Parcelable {
     private String firstname;
     private String lastname;
-    private String fullname;
+
     private DatePicker Birthdate;
     private int age;
     private ArrayList<String> gender;
@@ -23,31 +23,43 @@ public class CRACustomer  implements Parcelable {
     private float CPP;
     private float EI;
     private float CFRRSP;
+    private float Totaltaxincome;
+    private float TotalTaxPayed;
 
-    public CRACustomer(String firstname, String lastname, String fullname, DatePicker birthdate, int age, ArrayList<String> gender, int sinnumber, TextView taxdate, Float grosssincome, Float RRSP, float federal, float provincialtax, float CPP, float EI, float CFRRSP, float totaltaxincome, float totalTaxPayed) {
+    public CRACustomer(String firstname, String lastname, DatePicker birthdate, int age, ArrayList<String> gender, int sinnumber, TextView taxdate, Float grosssincome, Float RRSP, float federal, float provincialtax, float CPP, float EI, float CFRRSP, float totaltaxincome, float totalTaxPayed) {
         this.firstname = firstname;
         this.lastname = lastname;
-        this.fullname = fullname;
-        Birthdate = birthdate;
+
+        this. Birthdate = birthdate;
         this.age = age;
         this.gender = gender;
-        Sinnumber = sinnumber;
+       this. Sinnumber = sinnumber;
         this.taxdate = taxdate;
-        Grosssincome = grosssincome;
+        this.Grosssincome = grosssincome;
         this.RRSP = RRSP;
         this.federal = federal;
         this.provincialtax = provincialtax;
         this.CPP = CPP;
         this.EI = EI;
         this.CFRRSP = CFRRSP;
-        Totaltaxincome = totaltaxincome;
-        TotalTaxPayed = totalTaxPayed;
+        this.Totaltaxincome = totaltaxincome;
+       this. TotalTaxPayed = totalTaxPayed;
     }
 
-    private float Totaltaxincome;
-    private float TotalTaxPayed;
+
 
     protected CRACustomer(Parcel in) {
+        firstname = in.readString();
+        lastname = in.readString();
+
+        age =in.readInt();
+        gender =in.createStringArrayList();
+        Sinnumber = in.readInt();
+
+
+
+
+
     }
 
     public static final Creator<CRACustomer> CREATOR = new Creator<CRACustomer>() {
@@ -67,7 +79,150 @@ public class CRACustomer  implements Parcelable {
         return 0;
     }
 
+
+
+
+
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public DatePicker getBirthdate() {
+        return Birthdate;
+    }
+
+    public void setBirthdate(DatePicker birthdate) {
+        Birthdate = birthdate;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public ArrayList<String> getGender() {
+        return gender;
+    }
+
+    public void setGender(ArrayList<String> gender) {
+        this.gender = gender;
+    }
+
+    public int getSinnumber() {
+        return Sinnumber;
+    }
+
+    public void setSinnumber(int sinnumber) {
+        Sinnumber = sinnumber;
+    }
+
+    public TextView getTaxdate() {
+        return taxdate;
+    }
+
+    public void setTaxdate(TextView taxdate) {
+        this.taxdate = taxdate;
+    }
+
+    public Float getGrosssincome() {
+        return Grosssincome;
+    }
+
+    public void setGrosssincome(Float grosssincome) {
+        Grosssincome = grosssincome;
+    }
+
+    public Float getRRSP() {
+        return RRSP;
+    }
+
+    public void setRRSP(Float RRSP) {
+        this.RRSP = RRSP;
+    }
+
+    public float getFederal() {
+        return federal;
+    }
+
+    public void setFederal(float federal) {
+        this.federal = federal;
+    }
+
+    public float getProvincialtax() {
+        return provincialtax;
+    }
+
+    public void setProvincialtax(float provincialtax) {
+        this.provincialtax = provincialtax;
+    }
+
+    public float getCPP() {
+        return CPP;
+    }
+
+    public void setCPP(float CPP) {
+        this.CPP = CPP;
+    }
+
+    public float getEI() {
+        return EI;
+    }
+
+    public void setEI(float EI) {
+        this.EI = EI;
+    }
+
+    public float getCFRRSP() {
+        return CFRRSP;
+    }
+
+    public void setCFRRSP(float CFRRSP) {
+        this.CFRRSP = CFRRSP;
+    }
+
+    public float getTotaltaxincome() {
+        return Totaltaxincome;
+    }
+
+    public void setTotaltaxincome(float totaltaxincome) {
+        Totaltaxincome = totaltaxincome;
+    }
+
+    public float getTotalTaxPayed() {
+        return TotalTaxPayed;
+    }
+
+    public void setTotalTaxPayed(float totalTaxPayed) {
+        TotalTaxPayed = totalTaxPayed;
+    }
+
+    public static Creator<CRACustomer> getCREATOR() {
+        return CREATOR;
+    }
+
     @Override
-    public void writeToParcel(Parcel dest, int flags) {
+    public void writeToParcel(Parcel parcel, int flags) {
+        parcel.writeString(firstname);
+        parcel.writeString(lastname);
+
+        parcel.writeInt(age);
+        parcel.writeStringList(gender);
+        parcel.writeInt(Sinnumber);
     }
 }
