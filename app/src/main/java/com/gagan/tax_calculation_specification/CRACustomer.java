68cfaf7f7ceed224cc1db3c4a470a7927bcb7163
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-08 14:43:53 -0400

    [gk]making CRAcustom class parceable

--- app/src/main/java/com/gagan/tax_calculation_specification/CRACustomer.java
+++ app/src/main/java/com/gagan/tax_calculation_specification/CRACustomer.java
@@ -1,4 +1,30 @@
 package com.gagan.tax_calculation_specification;
 
-public class CRACustomer {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class CRACustomer  implements Parcelable {
+    protected CRACustomer(Parcel in) {
+    }
+
+    public static final Creator<CRACustomer> CREATOR = new Creator<CRACustomer>() {
+        @Override
+        public CRACustomer createFromParcel(Parcel in) {
+            return new CRACustomer(in);
+        }
+
+        @Override
+        public CRACustomer[] newArray(int size) {
+            return new CRACustomer[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
 }
