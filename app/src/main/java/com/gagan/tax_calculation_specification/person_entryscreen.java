52d06fdbc078acf7669d3364243d82ccc509b18f
gagandeep kaur <sherry2312.gk@gmail.com>
2020-04-08 01:34:22 -0400

    [GK]Date picker added for birthdate

--- app/src/main/java/com/gagan/tax_calculation_specification/person_entryscreen.java
+++ app/src/main/java/com/gagan/tax_calculation_specification/person_entryscreen.java
@@ -1,14 +1,59 @@
 package com.gagan.tax_calculation_specification;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class person_entryscreen extends AppCompatActivity {
+    private TextView txtDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_entryscreen);
-    }
-}
+       txtDate = findViewById(R.id.txtDate);
+
+    final Calendar myCalendar = Calendar.getInstance();
+
+
+    final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            String myFormat = "MM/dd/yy"; //In which you need put here
+            SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+            txtDate.setText(sdf.format(myCalendar.getTime()));
+
+        }
+
+    };
+
+       txtDate.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+            new DatePickerDialog(person_entryscreen.this, date, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        }
+
+    });
+}}
